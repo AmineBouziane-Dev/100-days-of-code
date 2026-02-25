a031e954a4f4afb4e62b2839022b0baccb0faf40
AmineBouziane-Dev <Amine.Aljazairi96@hotmail.com>
2026-02-25 06:45:15 +0000

    Band name generator

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Hello Sir/Madam.\nWelcome to the Band Name Generator.")
+#now lets ask certain questions to generate a name
+Childhood_City = input("What City did you grow up in?\n") #If you want the cursor to show on the next line for answering questions include the\n in the end
+Pet_Name = input("What is the name of your pet?\n")
+Band_Name = (Childhood_City+" "+Pet_Name) # look at the way you added a space here same as what we learned from print section.
+print("Your Band Name could be: "+ Band_Name + "!")
